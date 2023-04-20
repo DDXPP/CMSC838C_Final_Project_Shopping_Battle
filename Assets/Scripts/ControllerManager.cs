5232f6dd36c7ca3986649837ab7250ad815e1a19
Elliot Huang <Huang_E@outlook.com>
2023-04-20 15:34:03 -0400

    merge

---
+++ Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour
+{
+    GameObject grabbedObject;
+    public Rigidbody player;
+
+    void Start()
+    {
+        grabbedObject = null;
+    }
+    
+
+    void Update()
+    {
+        // use right hand trigger to grab object
+        if (GetRightTriggerPress() > 0.0f)
+        {   
+            GrabObject(GetRightTriggerPress());
+        }
+    }
+
+    Vector3 GetPointingDir() 
+    {
+        return transform.forward;
+    }
+
+    Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+
+
+    float GetRightTriggerPress()
+    {
+        return OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.RTouch);
+    }
+
+    void GrabObject(float pressedValue)
+    {
+        if (grabbedObject == null)
+        {
+            GameObject nearestObject = null;
+            float distance;
+            float nearestDistance = float.MaxValue;
+
+            Collider[] hitColliders = Physics.OverlapSphere(GetPosition(), 1.0f);
+            for (int i = 0; i < hitColliders.Length; i++)
+            {
+                distance = (GetPosition() - hitColliders[i].gameObject.transform.position).sqrMagnitude;
+                if (distance < nearestDistance) 
+                {
+                    if (hitColliders[i].gameObject.GetComponent<GrabbableObject>() != null) 
+                    {
+                        nearestDistance = distance; 
+                        nearestObject = hitColliders[i].gameObject;
+                    }
+                } 
+            }
+
+            if (nearestObject != null)
+                grabbedObject = nearestObject;  
+        }
+
+        grabbedObject.GetComponent<GrabbableObject>().Grab(pressedValue);
+    }
+}
---
+++ Assets/Scripts/GrabbableObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableObject : MonoBehaviour
+{
+    public GameObject controller;
+
+    bool isGrabbed;   
+
+    void Start()
+    {
+        isGrabbed = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrabbed) 
+        {
+            this.transform.parent = controller.transform;
+            this.GetComponent<Rigidbody>().isKinematic = true;
+            // this.GetComponent<Rigidbody>().useGravity = false;
+
+        } 
+        else
+        {
+            this.transform.parent = null;
+            this.GetComponent<Rigidbody>().isKinematic = false;
+            // this.GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LTouch);
+            // this.GetComponent<Rigidbody>().useGravity = true;
+        }   
+    }
+
+    public void Grab(float triggerPress)
+    {
+        if (triggerPress > 0.0f)
+            isGrabbed = true;
+        else 
+            isGrabbed = false;
+    }
+}
